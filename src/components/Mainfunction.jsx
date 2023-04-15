c2880769e6aae8c16ccfd9874ba59b1f33b15465
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-04-15 18:01:20 +0100

    Added threeJs and Theme switcher : bug

---
+++ src/components/Mainfunction.jsx
@@ -0,0 +1,12 @@
+import { FiberContainer } from "./FiberContainer.jsx";
+
+function App(){
+    return (
+    <div className="app">
+        
+        <FiberContainer />
+    </div>
+    )
+}
+
+export default App;
