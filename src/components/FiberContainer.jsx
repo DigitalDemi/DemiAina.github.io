9ddaf0b3a9cff66bf93fd92b9fba25ec7aba04bf
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-05-12 10:24:10 +0100

    feat: Added 3js background

--- src/components/FiberContainer.jsx
+++ src/components/FiberContainer.jsx
@@ -2,10 +2,10 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Scene } from "./Scene";
 
+
 export function FiberContainer() {
   return (
-    <Canvas camera={{position: [14.4666, 2.036, 5.5], fov: 40 }}>
-      
+    <Canvas className="align-middle top-0 left-0 fixed" camera={{position: [14.4666, 2.036, 5.5], fov: 40 }}>
       <Scene />
       
       <OrbitControls minDistance={1} maxDistance={200} />
--- src/components/Mainfunction.jsx
+++ src/components/Mainfunction.jsx
@@ -2,7 +2,7 @@ import { FiberContainer } from "./FiberContainer.jsx";
 
 function App(){
     return (
-    <div className="app flex w-[100vw] h-[80vh]">
+    <div className="app w-[100vw] h-[80vh]">
         
         <FiberContainer />
     </div>
