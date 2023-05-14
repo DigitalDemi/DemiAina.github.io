d56fd697f92ad4adb4ef96e37d0e67ca4bd3e48d
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-05-14 11:10:03 +0100

    Chore: Cleaned up styles: Feat: Added indexm pages for parts of the website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,13 @@ module.exports = {
 	darkMode : 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+			colors: {
+			blue: '#e5fffd',
+			black: '#0b0c12',
+			'light-blue': '#66fcf1',
+			'lighter-blue': '#202833',
+			grey: '#c5c6c8',
+		},
 	},
 	plugins: [],
 }
